cbf015691086d07a13bfe077fe27a2534eed987c
mehni <lozomehni28@gmail.com>
2024-01-20 12:54:27 +0100

    we add disc now bro

--- disc.py
+++ disc.py
@@ -20,3 +20,40 @@ class Disc_block(nn.Module):
     )
   def forward(self,x):
     return self.conv(x)
+
+
+class Discriminator(nn.Module):
+  def __init__(self,in_chans = 3,features = [64,128,256,512]):
+    super().__init__()
+    self.initial = nn.Sequential(
+        nn.Conv2d(
+            in_chans,
+            features[0],
+            kernel_size = 4,
+            stride = 2,
+            padding = 1,
+            padding_mode = "reflect"
+        ),
+        nn.LeakyReLU(0.2,inplace=True),
+    )
+    layers = []
+    in_chans = features[0]
+    for ft in features[1:]:
+      layers.append(
+          Disc_block(in_chans, ft, stride = 1 if ft == features[-1] else 2)
+      )
+      in_chans = ft
+      layers.append(
+          nn.Conv2d(
+              in_chans,
+              1,
+              kernel_size = 4,
+              stride = 1,
+              padding = 1,
+              padding_mode = "reflect"
+          )
+      )
+      self.model = nn.Sequential(*layers)
+  def forward(self,x):
+    x = self.initial(x)
+    return torch.sigmoid(self.model(x))
