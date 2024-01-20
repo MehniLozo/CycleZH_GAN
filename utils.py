190a41b6ad623fd71321304b073689f1c5732723
mehni <lozomehni28@gmail.com>
2024-01-20 13:11:33 +0100

    seeding techniques

--- utils.py
+++ utils.py
@@ -10,3 +10,22 @@ def save_milestone(model,optimizer, fn="ms.pth.tar"):
         "optimizer": optimizer.state_dict()
     }
     torch.save(ms, fn)
+
+def load_ms(fn, model, optimizer, lr):
+    print("---> Loading ...")
+    ms = torch.load(fn, map_location = config.DEVICE)
+    model.load_state_dict(ms["state_dict"])
+    optimizer.load_state_dict(ms["optimizer"])
+
+    for p_group in optimizer.param_groups:
+        p_group['lr'] = lr # we change the learning rate
+
+def seeding(seed=42):
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.becnhmark = False
